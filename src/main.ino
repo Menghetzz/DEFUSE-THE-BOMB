f07cf9605ec04c7fcce327dc1359e571e5bb0c83
Menghetz <manuelmenghetti0@gmail.com>
2025-11-07 17:49:26 +0100

    Add #define for led and button pins

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,17 @@
+#define blueLedPin 12
+#define blueLedButton 13
+
+#define whiteLedPin 10
+#define whiteLedButton 11
+
+#define yellowLedPin 8
+#define yellowLedButton 9
+
+#define greenLedPin 7
+#define greenLedButton 6
+
+#define redLedPin 7
+
 void setup() {
 
 }
