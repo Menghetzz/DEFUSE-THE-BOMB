2bc89d93019873918390dba8b16426405bf76ce9
Menghetz <manuelmenghetti0@gmail.com>
2025-11-08 09:57:20 +0100

    Add pinMode in setup

--- src/main.ino
+++ src/main.ino
@@ -1,18 +1,31 @@
-#define blueLedPin 12
-#define blueLedButton 13
+#define BLUE_LED_PIN 12
+#define BLUE_LED_BTN 13
 
-#define whiteLedPin 10
-#define whiteLedButton 11
+#define WHITE_LED_PIN 10
+#define WHITE_LED_BTN 11
 
-#define yellowLedPin 8
-#define yellowLedButton 9
+#define YELLOW_LED_PIN 8
+#define YELLOW_LED_BTN 9
 
-#define greenLedPin 7
-#define greenLedButton 6
+#define GREEN_LED_PIN 7
+#define GREEN_LED_BTN 6
 
-#define redLedPin 7
+#define RED_LED_PIN 2
+
+#define POT_PIN A0
 
 void setup() {
+    pinMode(BLUE_LED_PIN, OUTPUT);
+    pinMode(BLUE_LED_BTN, INPUT);
+    pinMode(WHITE_LED_PIN, OUTPUT);
+    pinMode(WHITE_LED_BTN, INPUT);
+    pinMode(YELLOW_LED_PIN, OUTPUT);
+    pinMode(YELLOW_LED_BTN, INPUT);
+    pinMode(GREEN_LED_PIN, OUTPUT);
+    pinMode(GREEN_LED_BTN, INPUT);
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(POT_PIN, INPUT);
+    Serial.begin(115200);
 
 }
 
