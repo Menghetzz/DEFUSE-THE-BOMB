bbdd32ab2499ff54ae20b71e7662862c763a134a
Fabian <fabiancalangiu5@gmail.com>
2025-11-16 14:44:22 +0100

    Added comments; Minor changes

--- src/main.ino
+++ src/main.ino
@@ -23,7 +23,7 @@ enum States {INITIAL, CREATE_PATTERN, GAME_ON};
 States currentState;
 
 enum Levels {L1, L2, L3};
-const int DIFFRANGE = 86;
+const int DIFFRANGE = 86; // Value given by dividing 255 / 3 (3 Levels)
 const int MAX_PATTERN = 100;
 
 int potValue;
@@ -57,6 +57,7 @@ void setup() {
     timer = new Timer();
 }
 
+// Function to determine the starting pattern based on the difficulty chosen
 void getPattern (){
     potValue = analogRead(POT_PIN);
     int diffMap = map(potValue, 0, 1023, 0, 255);
@@ -77,6 +78,7 @@ void getPattern (){
     }
 }
 
+// Function that checks if the user pattern corresponds to the game pattern
 bool checkPattern(){
     for(int i = 0; i < pattern; i++) {
         if(gamePattern[i] != userPattern[i]) {
@@ -95,11 +97,13 @@ void loop() {
             score = 0;
             pressed = digitalRead(GREEN_BTN);
             if(count == 0) {
-                Serial.println("Press the green button for start the game!");
+                Serial.println("Press the green button to start the game!");
                 count++;
             }
             delay(15);
-            if(pressed){
+
+            // Pressing the green button determines state change
+            if(pressed){ 
                 getPattern();
                 pressed = false;
                 currentState = CREATE_PATTERN;
@@ -107,12 +111,13 @@ void loop() {
             }
             digitalWrite(RED_LED, HIGH);
             break;
-        case CREATE_PATTERN:
+        case CREATE_PATTERN: 
             digitalWrite(RED_LED, LOW);
             delay(1000);
 
             indexUser = 0;
 
+            // Here the pattern gets created, saved and shown
             for(int i = 0; i < pattern; i++){
                 gamePattern[i] = LEDPINS[random(4)];
                 delay(1000);
@@ -121,12 +126,14 @@ void loop() {
                 digitalWrite(gamePattern[i], LOW);
             }
 
+            // Timer starts here
             timer->setupPeriod(6500);
             timerFlag = false;
             
             currentState = GAME_ON;
             break;
         case GAME_ON:
+            // Loop-parser for user input; all inputs get then saved in an array
             while(indexUser < pattern && !timerFlag) {
                 if(digitalRead(BLUE_BTN)) {
                     userPattern[indexUser] = BLUE_LED;
@@ -153,6 +160,9 @@ void loop() {
                     delay(150);
                 }
             }
+            
+            // Last state guard
+            // Inputing the correct pattern increases score and pattern difficulty
             
             if(checkPattern()) {
                 pattern++;
